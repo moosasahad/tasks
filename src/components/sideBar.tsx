df3fd674b33a5a513effd594ea6d646c2ce866a0
moosasahad <sahadmoosa018@gmail.com>
2025-02-17 13:40:24 +0530

    complete task

--- src/components/sideBar.tsx
+++ src/components/sideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SideBar() {
   return (
-    <div className='h-full bg-slate-200 text-black'>
+    <div className='h-full bg-slate-200'>
       <h1 className='w-52 text-xl p-2'>
       Sidebar
       </h1>
